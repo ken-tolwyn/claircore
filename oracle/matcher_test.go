bc600d28110ee4e4e192c8d59d173ea428a4ca4b
kenbugs <57542870+ken-tolwyn@users.noreply.github.com>
2024-09-01 22:47:31 +0200

    Update matcher_test.go
    Signed-off-by: kenbugs <57542870+ken-tolwyn@users.noreply.github.com>

--- oracle/matcher_test.go
+++ oracle/matcher_test.go
@@ -1,95 +1,104 @@
 package oracle
 
 import (
-    "context"
-    "testing"
+	"context"
+	"testing"
 
-    version "github.com/knqyf263/go-rpm-version"
-    "github.com/quay/claircore"
-    "github.com/stretchr/testify/assert"
+	"github.com/google/go-cmp/cmp"
+	"github.com/quay/zlog"
+
+	"github.com/quay/claircore"
+	"github.com/quay/claircore/test"
 )
 
 func TestVulnerableKsplice(t *testing.T) {
-    m := &Matcher{}
-    ctx := context.Background()
+	matcher := &Matcher{}
 
-    tests := []struct {
-        name     string
-        record   *claircore.IndexRecord
-        vuln     *claircore.Vulnerability
-        expected bool
-    }{
-        {
-            name: "pkgVer contains ksplice",
-            record: &claircore.IndexRecord{
-                Package: claircore.Package{
-                    Version: "1.0.0-ksplice",
-                    Arch:    "x86_64",
-                },
-            },
-            vuln: &claircore.Vulnerability{
-                Package: claircore.Package{
-                    Version: "1.0.0",
-                    Arch:    "x86_64",
-                },
-            },
-            expected: false,
-        },
-        {
-            name: "vulnVer contains ksplice",
-            record: &claircore.IndexRecord{
-                Package: claircore.Package{
-                    Version: "1.0.0",
-                    Arch:    "x86_64",
-                },
-            },
-            vuln: &claircore.Vulnerability{
-                Package: claircore.Package{
-                    Version: "1.0.0-ksplice",
-                    Arch:    "x86_64",
-                },
-            },
-            expected: false,
-        },
-        {
-            name: "both contain ksplice",
-            record: &claircore.IndexRecord{
-                Package: claircore.Package{
-                    Version: "1.0.0-ksplice",
-                    Arch:    "x86_64",
-                },
-            },
-            vuln: &claircore.Vulnerability{
-                Package: claircore.Package{
-                    Version: "1.0.0-ksplice",
-                    Arch:    "x86_64",
-                },
-            },
-            expected: true,
-        },
-        {
-            name: "neither contain ksplice",
-            record: &claircore.IndexRecord{
-                Package: claircore.Package{
-                    Version: "1.0.0",
-                    Arch:    "x86_64",
-                },
-            },
-            vuln: &claircore.Vulnerability{
-                Package: claircore.Package{
-                    Version: "1.0.0",
-                    Arch:    "x86_64",
-                },
-            },
-            expected: true,
-        },
-    }
+	testcases := []struct {
+		record *claircore.IndexRecord
+		vuln   *claircore.Vulnerability
+		name   string
+		want   bool
+	}{
+		{
+			name: "pkgVer contains ksplice",
+			record: &claircore.IndexRecord{
+				Package: &claircore.Package{
+					Version: "1.0.0-ksplice",
+					Arch:    "x86_64",
+				},
+			},
+			vuln: &claircore.Vulnerability{
+				Package: &claircore.Package{
+					Version: "1.0.0",
+					Arch:    "x86_64",
+				},
+			},
+			want: false,
+		},
+		{
+			name: "vulnVer contains ksplice",
+			record: &claircore.IndexRecord{
+				Package: &claircore.Package{
+					Version: "1.0.0",
+					Arch:    "x86_64",
+				},
+			},
+			vuln: &claircore.Vulnerability{
+				Package: &claircore.Package{
+					Version: "1.0.0-ksplice",
+					Arch:    "x86_64",
+				},
+			},
+			want: false,
+		},
+		{
+			name: "both contain ksplice",
+			record: &claircore.IndexRecord{
+				Package: &claircore.Package{
+					Name:    "kernel",
+					Version: "1.0.0-ksplice",
+				},
+			},
+			vuln: &claircore.Vulnerability{
+				Package: &claircore.Package{
+					Name:    "kernel",
+					Version: "1.0.0-ksplice",
+				},
+			},
+			want: true,
+		},
+		{
+			name: "neither contain ksplice",
+			record: &claircore.IndexRecord{
+				Package: &claircore.Package{
+					Name:    "kernel",
+					Version: "1.0.0",
+				},
+			},
+			vuln: &claircore.Vulnerability{
+				Package: &claircore.Package{
+					Name:    "kernel",
+					Version: "1.0.0",
+				},
+			},
+			want: true,
+		},
+	}
 
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-            result, err := m.Vulnerable(ctx, tt.record, tt.vuln)
-            assert.NoError(t, err)
-            assert.Equal(t, tt.expected, result)
-        })
-    }
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			got, err := matcher.Vulnerable(context.Background(), testcase.record, testcase.vuln)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !cmp.Equal(got, testcase.want) {
+				t.Error(cmp.Diff(got, testcase.want))
+			}
+		})
+	}
+}
+
+func TestMatcher(t *testing.T) {
+	test.RunMatcherTests(zlog.Test(context.Background(), t), t, "testdata/matcher", new(Matcher))
 }
